be8514a6e4b91aa9a9dcc19d21fa6a7219b46f06
Darshit Patel <darshit.patel2@dcmail.ca>
2022-06-30 16:01:00 -0400

    Debug mode is not working hence changed start object in package.json, also added node-fetch to fetch the data from the given link, implemented repo route logic to pass the fetched data once filtered

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,12 +1,28 @@
 import { Router, Request, Response } from 'express';
+const fetch = require('node-fetch');
 
 export const repos = Router();
 
-repos.get('/', async (_: Request, res: Response) => {
+repos.get('/', async (_: Request, res: Response, next) => {
   res.header('Cache-Control', 'no-store');
+
+  try {
+    const response = await fetch(
+      'https://api.github.com/users/silverorange/repos'
+    );
+    if (!response.ok) {
+      throw new Error('Request failed!');
+    }
+    const response_JSON = await response.json();
+    res.status(200);
+    res.header('Content-Type', 'application/json');
+    res.json({ repositories: response_JSON.filter((repo: any) => !repo.fork) });
+  } catch (err) {
+    next(err);
+  }
 
   res.status(200);
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  // res.json([]);
 });
