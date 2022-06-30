9e00ddb36083f6a8fefdcc5e37d3e30d3ec7a4eb
Darshit Patel <darshit.patel2@dcmail.ca>
2022-06-30 16:53:20 -0400

    Added useHttp as cusom hook because we will need to do multiple http calls to fetch repositories, commits, and readme file

---
+++ web/src/hooks/use-http.js
@@ -0,0 +1,29 @@
+import { useCallback, useState } from 'react';
+
+const useHttp = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchCall = useCallback(async (url, applyData, text) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        if (response.status === 404) {
+          throw new Error('No data found!');
+        }
+        throw new Error('Something went wrong');
+      }
+      const responseData = text ? await response.text() : await response.json();
+
+      applyData(responseData);
+    } catch (err) {
+      setError(err.message);
+    }
+  }, []);
+
+  return { isLoading, error, fetchCall };
+};
+
+export default useHttp;
